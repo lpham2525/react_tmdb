5c778f401acdc151d41d24352065feebfd958ef6
LanChi Pham <Lpham2525@gmail.com>
2021-10-12 09:37:56 -0400

    added firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,19 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch('https://react-meetup-d7c0b-default-rtdb.firebaseio.com/meetups.json', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
   return <section>
     <h1>Add New Meetup</h1>
-    <NewMeetupForm />
+    <NewMeetupForm onAddMeetup={addMeetupHandler}/>
   </section>
 }
 
