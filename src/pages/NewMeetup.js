9f55607f3674e6de4f43d98adc828e6cc35fade1
LanChi Pham <Lpham2525@gmail.com>
2021-10-12 09:41:52 -0400

    added history from react-router-dom

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,9 @@
+import { useHistory } from 'react-router-dom'
 import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 function NewMeetupPage() {
+  const history = useHistory()
+
   function addMeetupHandler(meetupData) {
     fetch('https://react-meetup-d7c0b-default-rtdb.firebaseio.com/meetups.json', {
       method: 'POST',
@@ -8,13 +11,17 @@ function NewMeetupPage() {
       headers: {
         'Content-Type': 'application/json'
       }
+    }).then(() => {
+      history.replace('/')
     })
   }
 
-  return <section>
-    <h1>Add New Meetup</h1>
-    <NewMeetupForm onAddMeetup={addMeetupHandler}/>
-  </section>
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+    </section>
+  )
 }
 
 export default NewMeetupPage
