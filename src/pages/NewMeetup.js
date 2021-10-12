ebfc2365cda10794f8a9f5facced9704c32b1267
LanChi Pham <Lpham2525@gmail.com>
2021-10-12 09:24:18 -0400

    added meetup form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,10 @@
-function NewMeetupPage() {
+import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
+function NewMeetupPage() {
+  return <section>
+    <h1>Add New Meetup</h1>
+    <NewMeetupForm />
+  </section>
 }
 
 export default NewMeetupPage
